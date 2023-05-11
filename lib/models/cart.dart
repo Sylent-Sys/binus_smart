b348505f32186fa016415a7bb95e91b439ec68d3
Sylent-Sys <renaldiapriyanto419@gmail.com>
2023-05-11 09:46:52 +0700

    make cart models

---
+++ lib/models/cart.dart
@@ -0,0 +1,39 @@
+class CartModel {
+  int id;
+  int userId;
+  String date;
+  List<CartProductModel> products;
+  CartModel({
+    required this.id,
+    required this.userId,
+    required this.date,
+    this.products = const [],
+  });
+  factory CartModel.fromJson(Map<String, dynamic> json) {
+    return CartModel(
+      id: json["id"],
+      userId: json["userId"],
+      date: json["date"],
+      products: List<CartProductModel>.from(
+        json["products"].map(
+          (x) => CartProductModel.fromJson(x),
+        ),
+      ),
+    );
+  }
+}
+
+class CartProductModel {
+  int productId;
+  int quantity;
+  CartProductModel({
+    required this.productId,
+    required this.quantity,
+  });
+  factory CartProductModel.fromJson(Map<String, dynamic> json) {
+    return CartProductModel(
+      productId: json["productId"],
+      quantity: json["quantity"],
+    );
+  }
+}
